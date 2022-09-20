6697c30d45b53ddce7cb79b7bb1e8413e1c3a58a
llopez <llopez@tropigas.com.pa>
2022-09-20 11:01:12 -0500

    Se completa la logica para el api gateway, ver archivos, AutorRemote, AutorModeloRemote, IAutorRemote, LibroHandler

--- TiendaServicios.Api.Gateway/Program.cs
+++ TiendaServicios.Api.Gateway/Program.cs
@@ -1,12 +1,24 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using TiendaServicios.Api.Gateway.ImplementRemote;
+using TiendaServicios.Api.Gateway.InterfaceRemote;
+using TiendaServicios.Api.Gateway.MessageHandler;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddOcelot();
+builder.Services.AddOcelot().AddDelegatingHandler<LibroHandler>();
+
+//Add scoped no funciona a nivel de middleware
+builder.Services.AddSingleton<IAutorRemote, AutorRemote>();
 
 builder.Configuration.AddJsonFile($"ocelot.json");
+
+ConfigurationManager configuration = builder.Configuration;
+
+builder.Services.AddHttpClient("AutorService", cfg => {
+    cfg.BaseAddress = new Uri(configuration["Services:Autor"]);
+});
 
 var app = builder.Build();
 
