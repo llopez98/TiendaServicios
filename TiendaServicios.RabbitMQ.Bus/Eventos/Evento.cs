579d157f363c3e8089f193fb11f985723ca7321b
llopez <llopez@tropigas.com.pa>
2022-09-20 15:30:14 -0500

    Se añade Libreria para RebbitMQ

---
+++ TiendaServicios.RabbitMQ.Bus/Eventos/Evento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaServicios.RabbitMQ.Bus.Eventos
+{
+    public abstract class Evento
+    {
+        public DateTime Timestamp { get; set; }
+        protected Evento() { 
+            Timestamp = DateTime.Now;
+        }
+    }
+}
