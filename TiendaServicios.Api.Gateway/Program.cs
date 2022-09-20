81b8259efd8348258d8d7291b31054a91beb6b11
llopez <llopez@tropigas.com.pa>
2022-09-20 08:09:41 -0500

    Api Gateway

---
+++ TiendaServicios.Api.Gateway/Program.cs
@@ -0,0 +1,26 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddOcelot();
+
+builder.Configuration.AddJsonFile($"ocelot.json");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+await app.UseOcelot();
+
+app.Run();
