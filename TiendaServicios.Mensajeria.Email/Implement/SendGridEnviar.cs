25c82f84ec35e72b2b0c7d67e53c3b1773d6325f
llopez <llopez@tropigas.com.pa>
2022-09-21 08:22:10 -0500

    Mensajeria usando SendGrid

---
+++ TiendaServicios.Mensajeria.Email/Implement/SendGridEnviar.cs
@@ -0,0 +1,34 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiendaServicios.Mensajeria.Email.Interface;
+using TiendaServicios.Mensajeria.Email.Modelo;
+
+namespace TiendaServicios.Mensajeria.Email.Implement
+{
+    internal class SendGridEnviar : ISendGridEnviar
+    {
+        public async Task<(bool resultado, string errorMessage)> EnviarEmail(SendGridData data)
+        {
+            try {
+                var sendGridCliente = new SendGridClient(data.SendGridAPIKey);
+                var destinatario = new EmailAddress(data.EmailDestinatario, data.NombreDestinatario);
+                var tituloEmail = data.Titulo;
+                var sender = new EmailAddress("correo", "nombre");
+                var contenidoMensaje = data.Contenido;
+
+                var objMensaje = MailHelper.CreateSingleEmail(sender, destinatario, tituloEmail, contenidoMensaje, contenidoMensaje);
+
+                await sendGridCliente.SendEmailAsync(objMensaje);
+
+                return (true, null);
+            } catch (Exception e) { 
+                return (false, e.Message);
+            }
+        }
+    }
+}
---
+++ TiendaServicios.Mensajeria.Email/Interface/ISendGridEnviar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiendaServicios.Mensajeria.Email.Modelo;
+
+namespace TiendaServicios.Mensajeria.Email.Interface
+{
+    public interface ISendGridEnviar
+    {
+        Task<(bool resultado, string errorMessage)> EnviarEmail(SendGridData data);
+    }
+}
