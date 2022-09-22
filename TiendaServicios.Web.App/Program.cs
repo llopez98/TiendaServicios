be375c6fe119a93bc497c8b180ffc379bc02fec5
llopez <llopez@tropigas.com.pa>
2022-09-22 07:23:19 -0500

    Se crea proyecto nuevo del aplicativo web para anexar autenticación y se elimina el anterior

---
+++ TiendaServicios.Web.App/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+using TiendaServicios.Web.App;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    // Configure your authentication provider options here.
+    // For more information, see https://aka.ms/blazor-standalone-auth
+    builder.Configuration.Bind("Local", options.ProviderOptions);
+});
+
+builder.Services.AddSyncfusionBlazor();
+
+await builder.Build().RunAsync();
